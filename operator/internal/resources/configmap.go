724295277ca8b26343e7a317d6d38839b9a7b368
Byran Carlock <bcarlock@mycarrier.io>
2026-04-06 17:29:25 -0500

    feat(resources): add label, configmap, serviceaccount, service, pdb builders
    Implement pure-function resource builders for the controllerutil.CreateOrUpdate
    mutate-function pattern:
    
    - labels.go: StandardLabels, SelectorLabels, DragonflyLabels
    - configmap.go: BuildConfigMap stores rendered krakend.json
    - serviceaccount.go: BuildServiceAccount with standard labels
    - service.go: BuildService with ClusterIP, configurable port
    - pdb.go: BuildPDB with maxUnavailable=1, selector labels

---
+++ operator/internal/resources/configmap.go
@@ -0,0 +1,30 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resources
+
+import (
+	v1alpha1 "github.com/mycarrier-devops/krakend-operator/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+)
+
+// BuildConfigMap mutates cm in place with the rendered krakend.json data.
+func BuildConfigMap(cm *corev1.ConfigMap, gw *v1alpha1.KrakenDGateway, jsonData []byte) {
+	cm.Labels = StandardLabels(gw)
+	cm.Data = map[string]string{
+		"krakend.json": string(jsonData),
+	}
+}
